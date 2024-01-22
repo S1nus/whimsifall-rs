aada5625a81e06ac22bc11dc562b960c27821460
Connor O'Hara <connor@switchboard.xyz>
2024-01-22 14:47:06 -0600

    tokio + submit blob function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::main;
+
+use celestia_rpc::{BlobClient, Client};
+use celestia_types::{Blob, nmt::Namespace};
+use celestia_types::blob::SubmitOptions;
+
+#[tokio::main]
+async fn main() {
+    submit_blob().await;
+}
+
+async fn submit_blob() {
+    // create a client to the celestia node
+    let token = std::env::var("CELESTIA_NODE_AUTH_TOKEN").expect("Token not provided");
+    let client = Client::new("ws://localhost:26658", Some(&token))
+        .await
+        .expect("Failed creating rpc client");
+
+    // create a blob that you want to submit
+    let my_namespace = Namespace::new_v0(&[1, 2, 3, 4, 5]).expect("Invalid namespace");
+    let blob = Blob::new(my_namespace, b"some data to store on blockchain".to_vec())
+        .expect("Failed to create a blob");
+
+    // submit it
+    client.blob_submit(&[blob], SubmitOptions::default())
+        .await
+        .expect("Failed submitting the blob");
 }
